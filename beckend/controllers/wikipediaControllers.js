32fc20babdf2c30aab090e16dcb0e41d466ba2a8
daviddagan <33395802+daviddagan@users.noreply.github.com>
2022-08-09 02:17:08 +0300

    end of fase 2

--- beckend/controllers/wikipediaControllers.js
+++ beckend/controllers/wikipediaControllers.js
@@ -1,19 +1,23 @@
 const scrapperService = require("../services/scrapper");
 
 function validArticleName(article) {
-    return /^[A-Za-z0-9-_]*$/.test(article);
+    return /^[\p{L}\p{N}\p{M}0-9-_]+$/u.test(article);
 }
 
 exports.getWikiArticleIntroduction = async function (req, res) {
     const articleName = req.params.id;
     try {
-        if (!validArticleName(articleName)) {
+        if (!validArticleName(articleName))
             return res.status(400).send({ message: "You may only accept article names that are comprised of letters, hyphens (-), underscores (_) and numbers" });
-        }
-        const selector = "#mw-content-text .mw-parser-output p:nth-of-type(2)" // wikipedia introduction selector
-        const wikiPage =  "en.wikipedia.org/wiki"
-        const articleIntroduction = await scrapperService.getArticleIntroduction(wikiPage,articleName,selector);
+
+        const language = req.headers["accept-language"] || "en" // there is no extra validation yet
+        const wikiPage = `${language}.wikipedia.org/wiki`
+        console.log(wikiPage)
+        const introductionSelector = "#mw-content-text .mw-parser-output p:nth-of-type(2)" // wikipedia introduction selector
+
+        const articleIntroduction = await scrapperService.getArticleIntroduction(wikiPage, articleName, introductionSelector);
         res.status(200).json(articleIntroduction);
+    
     } catch (e) {
         res.status(500).send({
             message: e.message || "failed load messages"
