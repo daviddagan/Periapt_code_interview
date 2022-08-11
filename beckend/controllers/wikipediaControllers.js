ea3554ee4246108ab0450de34a5b0e4b62b4aef8
daviddagan <33395802+daviddagan@users.noreply.github.com>
2022-08-11 12:24:32 +0300

    code review updates

--- beckend/controllers/wikipediaControllers.js
+++ beckend/controllers/wikipediaControllers.js
@@ -10,21 +10,25 @@ const favoriteLang = function (headers) {
 
     const token = headers['x-authentication']; // Get the token from 'x-authentication' header
     if (token) {
-        console.log(token," is not null");
+        console.log(token, " is not null");
         return userControllers.getUserLangByToken(token) ?? "en";
     }
 
     return "en";
 }
 
+
+
+
+
+
+
 exports.getWikiArticleIntroduction = async function (req, res) {
     const articleName = req.params.id;
+    const isNotWiki = req.quary.notwiki
     try {
         const language = favoriteLang(req.headers) // there is no extra validation yet
-        const wikiPage = `${language}.wikipedia.org/wiki`
-        const introductionSelector = "#mw-content-text .mw-parser-output p:nth-of-type(2)" // wikipedia introduction selector
-
-        const articleIntroduction = await scrapperService.getArticleIntroduction(wikiPage, articleName, introductionSelector);
+        const articleIntroduction = await scrapperService.getWikiArticleIntroduction(articleName, language);
         res.status(200).json(articleIntroduction);
 
     } catch (e) {
--- beckend/services/scrapper.js
+++ beckend/services/scrapper.js
@@ -7,7 +7,28 @@ const returnNowEpochUtC = () => {
     return Math.round(utcMilllisecondsSinceEpoch / 1000)
 }
 
-const getArticleIntroduction = async (sPage, articleName, selector) => {
+const getWikiArticleIntroduction = async (articleName,language) => {
+    const wikiPage = `${language}.wikipedia.org/wiki`
+    const introductionSelector = "#mw-content-text .mw-parser-output p:nth-of-type(2)" // wikipedia introduction selector
+    try {
+        return await scrapSegment(wikiPage,articleName,introductionSelector);
+    } catch (error) {
+        throw error 
+    }
+
+};
+const getNotWikiArticleIntroduction = async (articleName,language) => {
+    const wikiPage = `notwikipedia.com/wiki`
+    const introductionSelector = "#mw-content-text .mw-parser-output p:nth-of-type(2)" // wikipedia introduction selector
+    try {
+        return await scrapSegment(wikiPage,articleName,introductionSelector);
+    } catch (error) {
+        throw error 
+    }
+
+};
+
+const scrapSegment = async (sPage, articleName, selector) => {
     const browser = await puppeteer.launch(); // need to get out to init or somthing
     try {
         const page = await browser.newPage(); // need to get out to init or somthing
@@ -29,5 +50,5 @@ const getArticleIntroduction = async (sPage, articleName, selector) => {
 
 
 module.exports = {
-    getArticleIntroduction,
+    getWikiArticleIntroduction,
 };
